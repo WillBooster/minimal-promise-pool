6e04ffd3fce210795a8e38300b954240bdaf901e
Sakamoto, Kazunori <exkazuu@gmail.com>
2021-10-10 10:59:45 +0900

    feat: output .d.ts, .mjs and umd.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,21 @@
 import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
 
 const extensions = ['.mjs', '.js', '.json', '.ts'];
-const plugins = [babel({ extensions, babelHelpers: 'bundled', exclude: 'node_modules/**' })];
+const plugins = [babel({ extensions, babelHelpers: 'bundled', exclude: 'node_modules/**' }), terser()];
 
 export default {
   input: 'src/index.ts',
   output: [
     {
-      file: 'dist/index.js',
-      format: 'commonjs',
+      file: 'dist/index.min.mjs',
+      format: 'es',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.umd.min.js',
+      format: 'umd',
+      name: 'promisePool',
       sourcemap: true,
     },
   ],

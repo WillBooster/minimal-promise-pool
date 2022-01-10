19330885eec797bd1c2d4ac2eb9339c0ae21938c
Sakamoto, Kazunori <exkazuu@gmail.com>
2022-01-10 22:51:21 +0900

    test: refine test cases

--- __tests__/index.tests.ts
+++ __tests__/index.tests.ts
@@ -1,43 +1,13 @@
 import { PromisePool, sleep } from '../src';
 
-test('three heavy tasks', async () => {
+test('run three heavy tasks', async () => {
   const promisePool = new PromisePool(2);
   let startedCount = 0;
   let finishedCount = 0;
 
-  cancelled = false;
-  setTimeout(() => {
-    cancelled = true;
-  }, 4500);
-
   await promisePool.run(async () => {
     startedCount++;
-    const finished = await heavyTask();
-    if (finished) finishedCount++;
-  });
-  await promisePool.run(async () => {
-    startedCount++;
-    const finished = await heavyTask();
-    if (finished) finishedCount++;
-  });
-  await promisePool.run(async () => {
-    startedCount++;
-    const finished = await heavyTask();
-    if (finished) finishedCount++;
-  });
-
-  expect(startedCount).toBe(3);
-  expect(finishedCount).toBe(2);
-});
-
-test('one heavy and three light tasks', async () => {
-  const promisePool = new PromisePool(3);
-  let startedCount = 0;
-  let finishedCount = 0;
-
-  await promisePool.run(async () => {
-    startedCount++;
-    await sleep(100);
+    await sleep(1000);
     finishedCount++;
   });
   await promisePool.run(async () => {
@@ -45,6 +15,36 @@ test('one heavy and three light tasks', async () => {
     await sleep(1000);
     finishedCount++;
   });
+  await promisePool.run(async () => {
+    expect(startedCount).toBe(2);
+    expect(finishedCount).toBe(2);
+    startedCount++;
+    await sleep(1000);
+    finishedCount++;
+  });
+
+  expect(startedCount).toBe(3);
+  expect(finishedCount).toBe(3);
+});
+
+test('run one heavy and three light tasks', async () => {
+  const promisePool = new PromisePool(3);
+  let startedCount = 0;
+  let finishedCount = 0;
+
+  // light task 1
+  await promisePool.run(async () => {
+    startedCount++;
+    await sleep(100);
+    finishedCount++;
+  });
+  // heavy task 1
+  await promisePool.run(async () => {
+    startedCount++;
+    await sleep(1000);
+    finishedCount++;
+  });
+  // light task 2
   await promisePool.run(async () => {
     startedCount++;
     await sleep(100);
@@ -54,7 +54,10 @@ test('one heavy and three light tasks', async () => {
   while (finishedCount < 2) {
     await sleep(1);
   }
+  expect(startedCount).toBe(3);
+  expect(finishedCount).toBe(2);
 
+  // light task 3
   await promisePool.run(async () => {
     startedCount++;
     finishedCount++;
@@ -64,12 +67,3 @@ test('one heavy and three light tasks', async () => {
   expect(startedCount).toBe(4);
   expect(finishedCount).toBe(4);
 });
-
-let cancelled = false;
-async function heavyTask(): Promise<boolean> {
-  for (let i = 0; i < 6; i++) {
-    if (cancelled) return false;
-    await sleep(500);
-  }
-  return true;
-}
